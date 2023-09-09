047e3cf37cb07880cdc8451e1441939467ddb9e0
zhouchangsi <inbenbox@qq.com>
2023-09-09 09:35:30 +0800

    3 leetcodes

---
+++ src/leetcode144.cpp
@@ -0,0 +1,38 @@
+#include "common_headers.h"
+#include "utils/tree_node.hpp"
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> result;
+    if (root == nullptr) {
+      return result;
+    }
+
+    stack<TreeNode*> stack;
+    stack.push(root);
+    while (!stack.empty()) {
+      TreeNode* node = stack.top();
+      stack.pop();
+      if (node == nullptr) {
+        continue;
+      }
+      result.push_back(node->val);
+      stack.push(node->right);
+      stack.push(node->left);
+    }
+    return result;
+  }
+};
+
+TEST(leetcode144, solution) {
+  Solution solution;
+  {
+    TreeNode* root = TreeTest::from("1,null,2,3");
+    vector<int> result = solution.preorderTraversal(root);
+    vector<int> expected = {1, 2, 3};
+    EXPECT_EQ(result, expected);
+  }
+}
