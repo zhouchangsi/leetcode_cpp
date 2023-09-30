1594fb6e44ae72aa959fa692013de07ff5674d67
zhouchangsi <inbenbox@qq.com>
2023-09-30 19:23:03 +0800

    1 leetcodes

---
+++ src/leetcode763.cpp
@@ -0,0 +1,31 @@
+#include "common_headers.h"
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> partitionLabels(string s) {
+    int last[26];
+    int length = s.size();
+    for (int i = 0; i < length; i++) {
+      last[s[i] - 'a'] = i;
+    }
+    vector<int> partition;
+    int start = 0, end = 0;
+    for (int i = 0; i < length; i++) {
+      end = max(end, last[s[i] - 'a']);
+      if (i == end) {
+        partition.push_back(end - start + 1);
+        start = end + 1;
+      }
+    }
+    return partition;
+  }
+};
+
+TEST(leetcode763, solution) {
+  Solution s;
+  vector<int> result = s.partitionLabels("ababcbacadefegdehijhklij");
+  vector<int> expected = {9, 7, 8};
+  EXPECT_EQ(result, expected);
+}
